ef6b3ba1cf5bcbda083518dcd1b77b8749b37f00
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-11-03 13:56:34 -0500

    as

--- components/Login.tsx
+++ components/Login.tsx
@@ -49,7 +49,7 @@ const Login = ({ closeAllModals }: LoginProps) => {
     },
 
     validate: {
-      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email!'),
       password: (val) =>
         val.length <= 6
           ? 'Password should include at least 6 characters'
